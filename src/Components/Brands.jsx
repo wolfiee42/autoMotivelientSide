7e635483791f32f0e9100cf1fd5eebb7b805a278
wolfiee42 <saifalislam2022@gmail.com>
2023-10-20 09:04:59 +0600

    starting the day

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -1,19 +1,16 @@
 import PropTypes from 'prop-types';
-import { Link } from "react-router-dom";
 const Brands = ({ carBrandsinfo }) => {
 
     return (
         <div className='grid grid-cols-3 bg-white py-10 gap-4'>
             {
                 carBrandsinfo.map(brands => <>
-                    <Link to={'/brandedcar'}>
-                        <div className="card w-96 mx-auto bg-gray-200 shadow-xl hover:cursor-pointer">
-                            <figure><img className='w-[200px] h-[200px] p-4' src={brands.logoUrl} alt={brands.name} /></figure>
-                            <div className="card-body">
-                                <h2 className="card-title text-2xl">{brands.name}</h2>
-                            </div>
+                    <div className="card w-96 mx-auto bg-gray-200 shadow-xl hover:cursor-pointer">
+                        <figure><img className='w-[200px] h-[200px] p-4' src={brands.logoUrl} alt={brands.name} /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title text-2xl">{brands.name}</h2>
                         </div>
-                    </Link>
+                    </div>
                 </>)
             }
 
