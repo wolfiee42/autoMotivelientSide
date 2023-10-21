7d1b375aab155a235ae6da202176a163334f5496
wolfiee42 <saifalislam2022@gmail.com>
2023-10-21 08:09:29 +0600

    delte operation added

--- src/MyCart.jsx
+++ src/MyCart.jsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 const MyCart = () => {
     const data = useLoaderData()
     const [products, setProducts] = useState(data)
+
+
+    const handleDeletebtn = _id => {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your Product has been deleted.',
+                    'success'
+                )
+                fetch(`http://localhost:5000/productdetails/${_id}`, {
+                    method: "DELETE",
+                })
+                const remaining = products.filter(producty => producty._id !== _id);
+                setProducts(remaining);
+            }
+        })
+
+    }
+
+
     return (
         products.map(product => <>
             <div className='max-w-7xl mx-5 lg:mx-auto my-10'>
@@ -14,6 +45,7 @@ const MyCart = () => {
                         <p className="lg:text-lg font-semibold">{product.brand}</p>
                         <p className='text-xs lg:text-base'><span className="uppercase font-semibold">description : </span>  {product.description}</p>
                         <p className="uppercase font-bold">Price: ${product.price}</p>
+                        <button onClick={() => handleDeletebtn(product._id)} className="btn w-full bg-red-700 hover:bg-red-500 hover:text-black text-white">Delete</button>
                     </div>
                 </div>
             </div>
