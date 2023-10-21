8c4b2b8f3227b92c252df3f158f913030a24762b
wolfiee42 <saifalislam2022@gmail.com>
2023-10-21 10:31:07 +0600

    minor fix

--- src/MyCart.jsx
+++ src/MyCart.jsx
@@ -36,20 +36,26 @@ const MyCart = () => {
 
 
     return (
-        products.map(product => <>
-            <div className='max-w-7xl mx-5 lg:mx-auto my-10'>
-                <div className="card card-side flex-col lg:flex-row bg-gray-200 shadow-xl font-mono">
-                    <figure className="flex-1"><img className='p-10 lg:p-0' src={product.imageurl} alt={product.name} /></figure>
-                    <div className="card-body flex-1">
-                        <h2 className="card-title text-base lg:text-2xl uppercase">model :  {product.name}</h2>
-                        <p className="lg:text-lg font-semibold">{product.brand}</p>
-                        <p className='text-xs lg:text-base'><span className="uppercase font-semibold">description : </span>  {product.description}</p>
-                        <p className="uppercase font-bold">Price: ${product.price}</p>
-                        <button onClick={() => handleDeletebtn(product._id)} className="btn w-full bg-red-700 hover:bg-red-500 hover:text-black text-white">Delete</button>
-                    </div>
-                </div>
+        <div className="flex justify-center min-h-screen">
+            <div>
+                {
+                    products.map(product => <>
+                        <div className='max-w-7xl  mx-5 lg:mx-auto my-10'>
+                            <div className="card card-side flex-col lg:flex-row bg-gray-200 shadow-xl font-mono">
+                                <figure className="flex-1"><img className='p-10 lg:p-0' src={product.imageurl} alt={product.name} /></figure>
+                                <div className="card-body flex-1">
+                                    <h2 className="card-title text-base lg:text-2xl uppercase">model :  {product.name}</h2>
+                                    <p className="lg:text-lg font-semibold">{product.brand}</p>
+                                    <p className='text-xs lg:text-base'><span className="uppercase font-semibold">description : </span>  {product.description}</p>
+                                    <p className="uppercase font-bold">Price: ${product.price}</p>
+                                    <button onClick={() => handleDeletebtn(product._id)} className="btn w-full bg-red-700 hover:bg-red-500 hover:text-black text-white">Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    </>)
+                }
             </div>
-        </>)
+        </div>
 
     );
 };
