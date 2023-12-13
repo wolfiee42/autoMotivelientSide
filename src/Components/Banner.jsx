227ae49b7ea04d3ceeb957c5ef0a4966bcc4f299
wolfiee42 <saifalislam2022@gmail.com>
2023-12-13 14:04:24 +0600

    final touch

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,7 +7,6 @@ const   Banner = () => {
                     <h1 className=" mb-2 lg:mb-5 lg:text-5xl text-gray-500 lg:text-white font-semibold">Wolfie Auto Motive</h1>
                     <p className="lg:mb-5 text-gray-500 lg:text-white text-xs lg:text-base">Driving Dreams, Delivering Excellence</p>
                     <p className="lg:mb-5 text-gray-500 lg:text-white text-xs lg:text-base">Cruise into the future of automotive excellence at our cutting-edge car showroom. Wea re your gateway to a world of extraordinary driving experiences. Immerse yourself in a showroom filled with the latest, most advanced vehicles that seamlessly blend power, style, and innovation. Our mission is to turn your automotive dreams into reality. Step inside, and let us drive your passion for cars.</p>
-                    {/* <button className="btn btn-primary">Get Started</button> */}
                 </div>
             </div>
         </div>
